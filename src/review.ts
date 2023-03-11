1398eda4d356525f27c8f98c461a8af0b57d31b5
Harjot Gill <harjot.gill@fluxninja.com>
2023-03-11 09:02:55 -0800

    check lgtm

--- src/review.ts
+++ src/review.ts
@@ -288,8 +288,7 @@ export const codeReview = async (
           continue
         }
         next_review_ids = patch_ids
-        // if the response contains LGTM (case insensitive), then skip commenting
-        if (!options.review_comment_lgtm && response.match(/lgtm/i) !== null) {
+        if (!options.review_comment_lgtm && response.includes('LGTM')) {
           continue
         }
         try {
