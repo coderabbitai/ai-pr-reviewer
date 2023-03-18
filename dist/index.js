0b2a2ffb9e4e2f2ca3e196945036a71e57b5b77b
Harjot Gill <harjot.gill@fluxninja.com>
2023-03-18 10:09:09 -0700

    refactor

--- dist/index.js
+++ dist/index.js
@@ -27146,6 +27146,7 @@ ${COMMENT_TAG}`;
             const comments = await this.get_comments_at_line(pull_number, path, line);
             for (const comment of comments) {
                 if (comment.body && comment.body.includes(COMMENT_TAG)) {
+                    _actions_core__WEBPACK_IMPORTED_MODULE_0__.info(`Updating review comment ${comment.id}`);
                     await octokit.pulls.updateReviewComment({
                         owner: repo.owner,
                         repo: repo.repo,
