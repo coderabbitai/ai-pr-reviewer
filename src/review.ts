b21eb1c212c162c65e8c044be0415366fc9d9d7c
Harjot Gill <harjot.gill@fluxninja.com>
2023-03-24 13:51:30 -0700

    move comment

--- src/review.ts
+++ src/review.ts
@@ -67,11 +67,11 @@ export const codeReview = async (
 
   // skip files if they are filtered out
   const filter_selected_files = []
-  const filter_skipped_files = []
+  const filter_ignored_files = []
   for (const file of files) {
     if (!options.check_path(file.filename)) {
       core.info(`skip for excluded path: ${file.filename}`)
-      filter_skipped_files.push(file)
+      filter_ignored_files.push(file)
     } else {
       filter_selected_files.push(file)
     }
@@ -226,13 +226,23 @@ ${filename}: ${summary}
       const summarize_comment = `${summarize_final_response}
 
 ${
-  filter_skipped_files.length > 0
+  filter_ignored_files.length > 0
     ? `
 ---
 
-### Skipped files due to filter (${filter_skipped_files.length})
-- ${filter_skipped_files.map(file => file.filename).join('\n- ')}
+### Files ignored due to filter (${filter_ignored_files.length})
+- ${filter_ignored_files.map(file => file.filename).join('\n- ')}
 `
+    : ''
+}
+
+${
+  skipped_files_to_summarize.length > 0
+    ? `
+### Files not summarized due to max files limit (${
+        skipped_files_to_summarize.length
+      })
+- ${skipped_files_to_summarize.join('\n - ')}`
     : ''
 }
 
@@ -445,16 +455,11 @@ ${
 ${tag}
 
       ${
-        skipped_files_to_summarize.length > 0
-          ? `
-### Files not summarized (${skipped_files_to_summarize.length})
-- ${skipped_files_to_summarize.join('\n - ')}`
-          : ''
-      }
-      ${
         skipped_files_to_review.length > 0
           ? `
-### Files not reviewed (${skipped_files_to_review.length})
+### Files not reviewed due to max files limit (${
+              skipped_files_to_review.length
+            })
 - ${skipped_files_to_review.join('\n - ')}`
           : ''
       }
