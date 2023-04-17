ad5140154ae79864d47902d2fd7b6f24362826c1
Harjot Gill <harjot.gill@fluxninja.com>
2023-04-17 13:44:44 -0700

    incremental reviews

--- src/options.ts
+++ src/options.ts
@@ -146,13 +146,13 @@ export class TokenLimits {
 
   constructor(model = 'gpt-3.5-turbo') {
     if (model === 'gpt-4-32k') {
-      this.max_tokens = 32700
+      this.max_tokens = 32600
       this.response_tokens = 4000
     } else if (model === 'gpt-4') {
-      this.max_tokens = 8100
+      this.max_tokens = 8000
       this.response_tokens = 2000
     } else {
-      this.max_tokens = 4000
+      this.max_tokens = 3900
       this.response_tokens = 1000
     }
     this.request_tokens = this.max_tokens - this.response_tokens
