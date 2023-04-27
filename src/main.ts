8ade0d515d9cc145e55f357fe62f8e8089fae5de
AsafMah <asafmahlev@microsoft.com>
2023-04-27 09:56:28 +0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -26,7 +26,7 @@ async function run(): Promise<void> {
     getInput('openai_model_temperature'),
     getInput('openai_retries'),
     getInput('openai_timeout_ms'),
-    getInput('openai_concurrency_limit')
+    getInput('openai_concurrency_limit'),
     getInput('openai_base_url')
   )
 
