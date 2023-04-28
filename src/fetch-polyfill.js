e67125d8d7fc9b631e576f0c4c9a4d4547dfbab7
Harjot Gill <harjot.gill@fluxninja.com>
2023-04-28 15:55:20 -0700

    use p-retry

--- src/fetch-polyfill.js
+++
@@ -1,9 +0,0 @@
-// fetch-polyfill.js
-import fetch, {Headers, Request, Response} from 'node-fetch'
-
-if (!globalThis.fetch) {
-  globalThis.fetch = fetch
-  globalThis.Headers = Headers
-  globalThis.Request = Request
-  globalThis.Response = Response
-}
