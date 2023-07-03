2d33cb87ec1149d67207da1a3fb68cdf1ccfd80d
Gabriel Wilkes <gwilkes@redventures.com>
2023-07-03 11:31:17 -0500

    Fix best_practices script, leaving in prints to help debug

--- bin/best_practices.py
+++ bin/best_practices.py
@@ -19,6 +19,7 @@ def count_tokens_tiktoken(text):
 
 
 def split_text_into_chunks(text, max_tokens):
+    print(text)
     words = text.split()
     current_chunk = []
     chunks = []
@@ -55,7 +56,7 @@ def make_chat_completion_request(prompt, role = "You are a helpful assistant.", 
     total_tokens = response['usage']['total_tokens']
     return result, prompt_tokens, completion_tokens, total_tokens
 
-def make_gpt4_completion_request(prompt, role = "You are a helpful assistant.", max_tokens=200, temperature=0.7):
+def make_gpt4_completion_request(prompt, role = "You are a helpful assistant.", max_tokens=200, temperature=0.1):
     response = openai.ChatCompletion.create(
         model="gpt-4",
         messages=[
@@ -81,7 +82,7 @@ def best_practices_prompt(commit):
     return prompt, system_notes
 
 def condense_best_practices_prompt(design_patterns):
-    system_notes = '''I have assembled a list of DESIGN PATTERNS used in my repository. The problem is that the DESIGN PATTERNS is too long. Identify the MOST IMPORTANT DESIGN PATTERNS based on DESIGN PATTERNS that you find are either duplicated or are crucial to coding in this repository. Make sure to include the framework within the MOST IMPORTANT DESIGN PATTERNS.'''
+    system_notes = '''I have assembled a list of DESIGN PATTERNS used in my repository. The problem is that the DESIGN PATTERNS is too long. Identify the MOST IMPORTANT DESIGN PATTERNS based on DESIGN PATTERNS that you find are either duplicated or are crucial to coding in this repository. Make sure to include the framework and any unique style preferences within the MOST IMPORTANT DESIGN PATTERNS.'''
 
     prompt = f'''DESIGN PATTERNS:
     {design_patterns}
@@ -97,20 +98,23 @@ headers = {
 
 # Fetch the last 10 pull requests
 url = f"https://api.github.com/repos/{GITHUB_OWNER}/{GITHUB_REPO}/pulls?state=closed&per_page=10"
+print(url)
 response = requests.get(url, headers=headers)
 pull_requests = response.json()
-
+print(pull_requests)
 # Add diffs to list
 commits_list = []
 for pr in pull_requests:
     diff_url = pr["diff_url"]
+    print(diff_url)
     diff_response = requests.get(diff_url, headers=headers)
-    print(diff_response)
-    commits_list.append(diff_response)
+    diff_response = diff_response.text
+    # limit diffs to 30,000 tokens
+    if count_tokens_tiktoken(diff_response) < 30000:
+        print(diff_response)
+        commits_list.append(diff_response)
 
-
-
-
+print(commits_list)
 def identify_best_practices(commits_list):
     best_practices_list = []
     for commit in commits_list:
@@ -120,13 +124,14 @@ def identify_best_practices(commits_list):
         # calculate prompt length without adding diff file
         prompt, system_notes = best_practices_prompt('')
         prompt_len = count_tokens_tiktoken(prompt) + count_tokens_tiktoken(system_notes)
-        max_tokens = 8192 - prompt_len - 200 #for completion
+        max_tokens = 8192 - prompt_len - 400 #for completion
         # split diff file based on token space
         commit_segments = split_text_into_chunks(commit, max_tokens)
         for segment in commit_segments:
             prompt, system_notes = best_practices_prompt(segment)
-            result, prompt_tokens, completion_tokens, tokens = make_chat_completion_request(prompt, role=system_notes)
+            result, prompt_tokens, completion_tokens, tokens = make_chat_completion_request(prompt, role=system_notes, max_tokens=400)
             best_practices_list.append(result)
+            print(result)
 
             total_gpt4_prompt_tokens += prompt_tokens
             total_gpt4_completion_tokens += completion_tokens
