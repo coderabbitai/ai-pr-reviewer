b02daa38c444e6209e27dd949deaf8bd350a91ec
Harjot Gill <harjot.gill@fluxninja.com>
2023-07-24 19:41:56 -0700

    github concurrency limits

--- src/options.ts
+++ src/options.ts
@@ -17,6 +17,7 @@ export class Options {
   openaiRetries: number
   openaiTimeoutMS: number
   openaiConcurrencyLimit: number
+  githubConcurrencyLimit: number
   lightTokenLimits: TokenLimits
   heavyTokenLimits: TokenLimits
   apiBaseUrl: string
@@ -35,7 +36,8 @@ export class Options {
     openaiModelTemperature = '0.0',
     openaiRetries = '3',
     openaiTimeoutMS = '120000',
-    openaiConcurrencyLimit = '4',
+    openaiConcurrencyLimit = '6',
+    githubConcurrencyLimit = '6',
     apiBaseUrl = 'https://api.openai.com/v1'
   ) {
     this.debug = debug
@@ -52,6 +54,7 @@ export class Options {
     this.openaiRetries = parseInt(openaiRetries)
     this.openaiTimeoutMS = parseInt(openaiTimeoutMS)
     this.openaiConcurrencyLimit = parseInt(openaiConcurrencyLimit)
+    this.githubConcurrencyLimit = parseInt(githubConcurrencyLimit)
     this.lightTokenLimits = new TokenLimits(openaiLightModel)
     this.heavyTokenLimits = new TokenLimits(openaiHeavyModel)
     this.apiBaseUrl = apiBaseUrl
@@ -73,6 +76,7 @@ export class Options {
     info(`openai_retries: ${this.openaiRetries}`)
     info(`openai_timeout_ms: ${this.openaiTimeoutMS}`)
     info(`openai_concurrency_limit: ${this.openaiConcurrencyLimit}`)
+    info(`github_concurrency_limit: ${this.githubConcurrencyLimit}`)
     info(`summary_token_limits: ${this.lightTokenLimits.string()}`)
     info(`review_token_limits: ${this.heavyTokenLimits.string()}`)
     info(`api_base_url: ${this.apiBaseUrl}`)
