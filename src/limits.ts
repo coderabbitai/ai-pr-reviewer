1af60cec1f07b563c4be79bb94fb88ceaa760d10
darshil2 <darshil.shah@gtmhub.com>
2023-07-27 12:34:35 +0530

    upated openai max token limits for 16k model

--- src/limits.ts
+++ src/limits.ts
@@ -15,6 +15,9 @@ export class TokenLimits {
     } else if (model === 'gpt-4') {
       this.maxTokens = 8000
       this.responseTokens = 2000
+    } else if (model === 'gpt-35-turbo-16k') {
+        this.maxTokens = 16000
+        this.responseTokens = 4000
     } else {
       this.maxTokens = 4000
       this.responseTokens = 1000
