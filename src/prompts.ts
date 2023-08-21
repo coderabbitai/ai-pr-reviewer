164b539029d6ae4a209fb6f02af421d6a22abf2a
Harjot Gill <harjot.gill@fluxninja.com>
2023-08-21 07:58:45 -0700

    update prompt

--- src/prompts.ts
+++ src/prompts.ts
@@ -99,62 +99,25 @@ $description
 $short_summary
 \`\`\`
 
-## Instructions
+## IMPORTANT Instructions
 
-The format for changes provided in the example below consists of 
-multiple change sections, each containing a new hunk (annotated with 
-line numbers), an old hunk, and optionally, existing comment chains. 
-Note that the old hunk code has been replaced by the new hunk. Some 
-lines on the new hunk may be annotated with line numbers.
+Input: New hunks annotated with line numbers and old hunks (replaced code). Hunks represent incomplete code fragments.
+Additional Context: PR title, description, summaries and comment chains.
+Task: Review new hunks for substantive issues using provided context and respond with comments if necessary.
+Output: Review comments in markdown with exact line number ranges in new hunks. Start and end line numbers must be within the same hunk. For single-line comments, start=end line number. Must use example response format below.
+Use fenced code blocks using the relevant language identifier where applicable.
+Don't annotate code snippets with line numbers. Format and indent code correctly.
+Do not use \`suggestion\` code blocks.
+For fixes, use \`diff\` code blocks, marking changes with \`+\` or \`-\`. The line number range for comments with fix snippets must exactly match the range to replace in the new hunk.
 
-Your task is to meticulously perform line-by-line review of new hunks, 
-identifying substantial issues only. Respond only in the below example format, 
-consisting of review sections. Each review section must have a line number range 
-and a review comment for that range. Use separator after each review section. 
-Line number ranges for each review section must be within the range of a specific 
-new hunk. Start line number must belong to the same hunk as the end line number.
-Provide the exact line number range (inclusive) for each review comment. To leave 
-a review comment on a single line, use the same line number for start and end.
-
-Take into consideration the context provided by old hunks, comment threads, and 
-file content during your review. Remember, the hunk under review is a fragment of a 
-larger codebase and may not show all relevant sections, such as definitions, 
-imports, or usage of functions or variables. Expect incomplete code fragments or 
-references to elements defined beyond the provided context. Do NOT flag missing 
-definitions, imports, or usages unless the context strongly suggests an issue. 
-Do NOT restate information readily apparent in the code or the pull request. 
-Do NOT provide general feedback, summaries, explanations of changes, or praises 
-for making good additions. Do NOT question the developer's intentions behind the 
-changes or warn them about potential compatibility issues with other dependencies. 
-Avoid making assumptions about broader impacts beyond the given context or the 
-necessity of the changes. Do NOT request the developer to review their changes. 
-Given your knowledge may be outdated, it is essential to trust the developer when 
-they appear to utilize newer APIs and methods. Presume the developer has 
-exhaustively tested their changes and is fully aware of their system-wide 
-implications. Focus solely on offering specific, objective insights based on the 
-actual code and refrain from making broad comments about potential impacts on 
-the system.
-
-Use GitHub flavored markdown format for review comment text 
-and fenced code blocks for code snippets using the relevant 
-language identifier. Do NOT annotate the code snippet with 
-line numbers. The code snippet must be correctly 
-formatted & indented.
-
-If applicable, you may provide a replacement snippet to fix 
-issues within a hunk by using \`diff\` code blocks, clearly 
-marking the lines that need to be added or removed with \`+\` 
-and \`-\` annotations. The line number range for the review 
-comment that includes a replacement snippet must precisely map 
-to the line number range that has to be completely replaced 
-within a hunk. Do NOT use \`suggestion\` code blocks for
-replacement snippets.
+- Do NOT provide general feedback, summaries, explanations of changes, or praises 
+  for making good additions. 
+- Focus solely on offering specific, objective insights based on the 
+  given context and refrain from making broad comments about potential impacts on 
+  the system or question intentions behind the changes.
 
 If there are no issues found on a line range, you MUST respond with the 
 text \`LGTM!\` for that line range in the review section. 
-
-Reflect on your comments thoroughly before posting them to 
-ensure accuracy and compliance with the above guidelines.
 
 ## Example
 
