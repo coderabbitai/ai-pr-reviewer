9a069e796f64d02e6f3a88a4e986ac9f04d3ae3d
tmokmss <tmokmss@users.noreply.github.com>
2023-10-07 21:51:51 +0900

    revert token count

--- src/tokenizer.ts
+++ src/tokenizer.ts
@@ -1,5 +1,13 @@
-import {countTokens} from '@anthropic-ai/tokenizer'
+// eslint-disable-next-line camelcase
+import {get_encoding} from '@dqbd/tiktoken'
+
+const tokenizer = get_encoding('cl100k_base')
+
+export function encode(input: string): Uint32Array {
+  return tokenizer.encode(input)
+}
 
 export function getTokenCount(input: string): number {
-  return countTokens(input)
+  input = input.replace(/<\|endoftext\|>/g, '')
+  return encode(input).length
 }
